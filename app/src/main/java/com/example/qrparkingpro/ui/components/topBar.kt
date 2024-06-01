c0f18a2fb0627b6b1e92afd61e1dfb2d06b54006
brianduong <quan.duongdong@hcmut.edu.vn>
2024-06-01 18:20:13 +0700

    fixed

--- app/src/main/java/com/example/qrparkingpro/ui/components/topBar.kt
+++ app/src/main/java/com/example/qrparkingpro/ui/components/topBar.kt
@@ -1,5 +1,6 @@
 package com.example.qrparkingpro.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -7,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,31 +20,37 @@ fun TopBar(navController: NavController, title: String) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(72.dp), // Điều chỉnh chiều cao của TopBar tại đây
-        contentAlignment = Alignment.Center
+            .height(72.dp)
     ) {
         TopAppBar(
             backgroundColor = Color(0xFF1877F2),
             elevation = 0.dp,
-            navigationIcon = {
-                IconButton(onClick = { navController.popBackStack() }) {
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Sử dụng Box để chứa các thành phần trong TopAppBar
+            Box(modifier = Modifier.fillMaxSize()) {
+                // Nút điều hướng nằm bên trái
+                IconButton(
+                    onClick = { navController.popBackStack() },
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
                     Icon(
                         painter = painterResource(id = R.drawable.back),
                         contentDescription = "Back",
                         tint = Color.White
                     )
                 }
-            },
-            title = {
-                    Text(
-                        text = title,
-                        color = Color.White,
-                        textAlign = TextAlign.Center,
-                        fontSize = 25.sp
-                    )
 
-            },
-            modifier = Modifier.fillMaxSize() // Đảm bảo TopAppBar chiếm toàn bộ chiều cao của Box
-        )
+                // Tiêu đề nằm chính giữa
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    fontSize = 25.sp,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
     }
 }
